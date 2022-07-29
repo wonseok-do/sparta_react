6329f2febbc2cc7c835c812663ead8d80e5bab1c
wonseok-do <wsdo970@gmail.com>
2022-07-29 20:58:15 +0900

    scrollBar 추가

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     // 스프레드 문법! 기억하고 계신가요? :)
     // 원본 배열 list에 새로운 요소를 추가해주었습니다.
     // setList([...list, text.current.value]);
-    dispatch(createBucket(text.current.value));
+    dispatch(createBucket({ text: text.current.value, completed: false }));
   };
   return (
     <div className="App">
@@ -43,6 +43,13 @@ function App() {
         <input type="text" ref={text} />
         <button onClick={addBucketList}>추가하기</button>
       </Input>
+      <button
+        onClick={() => {
+          window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        }}
+      >
+        위로 가기
+      </button>
     </div>
   );
 }
