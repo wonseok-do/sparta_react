ad27598a2b9e0b671746217fd423ca7fbbda820f
wonseok-do <wsdo970@gmail.com>
2022-07-31 23:52:07 +0900

    thunk 추가

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import bucket from "./modules/bucket";
+import thunk from "redux-thunk";
 
+const middlewares = [thunk];
 const rootReducer = combineReducers({ bucket });
+const enhancer = applyMiddleware(...middlewares);
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
