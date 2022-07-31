5956d2c0b8dd1ebc6cc8ff759c92eb7f431cb922
wonseok-do <wsdo970@gmail.com>
2022-07-31 23:53:20 +0900

    load, update, delete, create 추가

--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -1,6 +1,17 @@
 // bucket.js
-
+import { db } from "../../firebase";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { async } from "@firebase/util";
 // Actions
+const LOAD = "bucket/LOAD";
 const CREATE = "bucket/CREATE";
 const UPDATE = "bucket/UPDATE";
 const DELETE = "bucket/DELETE";
@@ -15,6 +26,9 @@ const initialState = {
 };
 
 // Action Creators
+export function loadBucket(bucket_list) {
+  return { type: LOAD, bucket_list };
+}
 export function createBucket(bucket) {
   return { type: CREATE, bucket };
 }
@@ -24,10 +38,66 @@ export function updateBucket(bucket_index) {
 export function deleteBucket(bucket_index) {
   return { type: DELETE, bucket_index };
 }
+// middlewares
+export const loadBucketFB = () => {
+  return async function (dispatch) {
+    const bucket_data = await getDocs(collection(db, "bucket"));
+    console.log(bucket_data);
+
+    let bucket_list = [];
+    bucket_data.forEach((bucket) => {
+      console.log(bucket.data());
+      bucket_list.push({ id: bucket.id, ...bucket.data() });
+    });
+    console.log(bucket_list);
+    dispatch(loadBucket(bucket_list));
+  };
+};
+
+export const addBucketListFB = (bucket) => {
+  return async function (dispatch) {
+    const docRef = await addDoc(collection(db, "bucket"), bucket);
+    // const _bucket = await getDoc(docRef); // 1번째 방법
+    // const bucket_data = { id: _bucket.id, ..._bucket.data() };
+    // console.log(bucket_data);
+    const bucket_data = { id: docRef.id, ...bucket }; // 2번째 방법
+    dispatch(createBucket(bucket_data));
+  };
+};
+export const updateBucketFB = (bucket_id) => {
+  return async function (dispatch, getState) {
+    const docRef = doc(db, "bucket", bucket_id);
+    await updateDoc(docRef, { completed: true });
+    const _bucket_list = getState().bucket.list;
+    const bucket_index = _bucket_list.findIndex((b) => {
+      return b.id === bucket_id;
+    });
+    dispatch(updateBucket(bucket_index));
+  };
+};
+
+export const deleteBucketFB = (bucket_id) => {
+  return async function (dispatch, getState) {
+    if (!bucket_id) {
+      window.alert("아이디가 없네요!");
+      return;
+    }
+    const docRef = doc(db, "bucket", bucket_id);
+    await deleteDoc(docRef);
+    const _bucket_list = getState().bucket.list;
+    const bucket_index = _bucket_list.findIndex((b) => {
+      return b.id === bucket_id;
+    });
+    dispatch(deleteBucket(bucket_index));
+  };
+};
 
 // Reducer
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case "bucket/LOAD": {
+      return { list: action.bucket_list };
+    }
     case "bucket/CREATE": {
       const new_bucket_list = [...state.list, action.bucket];
       return { list: new_bucket_list };
